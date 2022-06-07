ac0b6c1df19483af83c72445a53f979ab05aea37
RadhikaPPatel <radhika.patel@ajackus.com>
2022-06-07 16:33:40 +0530

    Add subscriber to admin panel

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from core.models import CoreUser, CoreGroup, CoreSites, EmailTemplate, Industry, LogicModule, Organization
+from core.models import CoreUser, CoreGroup, CoreSites, EmailTemplate, Industry, LogicModule, Organization, Subscriber
 
 
 class LogicModuleAdmin(admin.ModelAdmin):
@@ -68,3 +68,4 @@ admin.site.register(CoreUser, CoreUserAdmin)
 admin.site.register(CoreSites, CoreSitesAdmin)
 admin.site.register(EmailTemplate, EmailTemplateAdmin)
 admin.site.register(Industry)
+admin.site.register(Subscriber)
